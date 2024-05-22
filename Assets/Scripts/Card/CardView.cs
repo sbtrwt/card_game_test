9377323492c0dd21b79806b141ee237eec936a8a
shobhit-rawat <sbtrwt.se@gmail.com>
2024-05-22 18:30:15 +0530

    SO changes

--- Assets/Scripts/Card/CardView.cs
+++ Assets/Scripts/Card/CardView.cs
@@ -66,7 +66,7 @@ namespace CardGame.Card
         
         public void SetCardFaceSprite()
         {
-            controller.SetCardFaceSprite(faceSpriteRenderer);
+            controller?.SetCardFaceSprite(faceSpriteRenderer);
 
         }
     }
