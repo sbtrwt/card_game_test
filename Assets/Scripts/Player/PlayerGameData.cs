95170142924c8837b1e7ff0543d537165432c3b7
shobhit-rawat <sbtrwt.se@gmail.com>
2024-05-23 12:05:03 +0530

    Added History action

---
+++ Assets/Scripts/Player/PlayerGameData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Player
+{
+
+    [Serializable]
+    public class PlayerGameData
+    {
+        public string GameShortName;
+        public DateTime PlayedOn;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerGameHistory.cs
@@ -0,0 +1,34 @@
+﻿using CardGame.Main;
+using CardGame.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardGame.Player
+{
+    [Serializable]
+    public class PlayerGameHistory
+    {
+        public List<PlayerGameData> PlayerGameDataHistory;
+        public PlayerGameHistory()
+        {
+            PlayerGameDataHistory = new List<PlayerGameData>();
+        }
+
+        public List<PlayerGameData> GetSavedHistory() 
+        {
+            string historyText = SecurePlayerPrefs.GetString(GlobalConstant.KEY_PLAYERDATAHISTORY);
+            JsonUtility.FromJsonOverwrite(historyText, PlayerGameDataHistory);
+            return PlayerGameDataHistory;
+        }
+
+        public void AddSavedHistory(PlayerGameData newGameEntry)
+        {
+            PlayerGameDataHistory = GetSavedHistory();
+            PlayerGameDataHistory.Add(newGameEntry);
+
+            SecurePlayerPrefs.SetString(GlobalConstant.KEY_PLAYERDATAHISTORY, JsonUtility.ToJson(PlayerGameDataHistory));
+           
+        }
+    }
+}
